6d2c506b3438941911ada7074f61c706955fc554
JayaPraveen29 <jayapg2005@gmail.com>
2026-08-12 12:49:18 +0530

    Made change to casing

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import { Navigate, Route, Routes } from "react-router-dom";
 import Sidebar from "./components/Sidebar";
 import Dashboard from "./pages/Dashboard/Dashboard";
-import POSheet from "./pages/po/POSheet";
+import POSheet from "./pages/PO/POSheet";
 import ABSReport from "./pages/ABSReport/ABSReport";
 import "./App.css";
 
